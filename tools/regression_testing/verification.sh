0c90b7c36c152536143bbce9b081921462d9638b
Matthew Fortier <fortier.matt@gmail.com>
2019-10-12 19:21:35 -0700

    applied CI_BUILD_NAME to new checksum protocol

--- tools/regression_testing/verification.sh
+++ tools/regression_testing/verification.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 testcase=$1
 bdir=/space/hall1/sitestore/eccc/crd/ccrp/scrd530/classic_checksums
+rf=${CI_BUILD_REF:0:8}
+mkdir -p $bdir/checksums/$rf
+mv $bdir/$testcase/checksums.csv $bdir/checksums/$rf/${testcase}_checksums.csv
 
-
-casediff=$( diff $bdir/$testcase/checksums.csv $bdir/baseline/${testcase}_checksums.csv )
+casediff=$( diff $bdir/checksums/$rf/${testcase}_checksums.csv $bdir/checksums/baseline/${testcase}_checksums.csv )
 
 if [ "$casediff" != "" ]; then
-  diff $bdir/$testcase/checksums.csv $bdir/checksums/baseline/${testcase}_checksums.csv
+  diff $bdir/checksums/$rf/${testcase}_checksums.csv $bdir/checksums/baseline/${testcase}_checksums.csv
   echo "$testcase checksum verification has failed"
   exit 1
 fi
 echo "$testcase checksum verification passed"
-mkdir -p $bdir/checksums/$CI_COMMIT_SHORT_SHA
-mv $bdir/$testcase/checksums.csv $bdir/checksums/$CI_COMMIT_SHORT_SHA/${testcase}_checksums.csv
 exit 0
